a15fcc999332de184f3b7e8266199743073648a4
Minhao Han <hannidiot2018@gmail.com>
2022-04-15 14:13:31 +0100

    add basic test structure

---
+++ test/bbox_tracker_tests.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE BboxTrackerTest
+
+#include <boost/test/unit_test.hpp>
+
+#include "bbox_tracker.h"
+
+BOOST_AUTO_TEST_CASE(TrackingTest) {
+
+}
+
+BOOST_AUTO_TEST_CASE(NewAppearTest) {
+
+}
+
+BOOST_AUTO_TEST_CASE(DisappearTest) {
+
+}
