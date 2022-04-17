6070962d262d61750dfb3e8775450708d082d620
Minhao Han <hannidiot2018@gmail.com>
2022-04-17 12:16:31 +0100

    feature complete, branch closed

--- src/bbox_tracker.h
+++ src/bbox_tracker.h
@@ -10,15 +10,24 @@ class BboxTracker {
 public:
     BboxTracker(TrackerSetting&& setting);
 
-    void update(cv::Point2f&& midpoint);
+    void update(const std::vector<FaceInfo>& point);
     const std::vector<TrackingObj> &getTrackingList() const;
 private:
     uint nextId;
     TrackerSetting trackerSetting;
     std::vector<TrackingObj> trackingList;
 
-    void registerObj(cv::Point2f&& midpoint);
+    void registerObj(cv::Point2f* midpoint);
     void deregisterObj(uint objectId);
+    static double calcDistance(const cv::Point2f& pt1, const cv::Point2f& pt2);
+    static cv::Point2f getMidpoint2f(const cv::Point2f& pt1, const cv::Point2f& pt2);
+    static cv::Point2f getMidpoint2f(float x1, float x2, float y1, float y2);
+};
+
+struct PointDistance {
+    TrackingObj* src;
+    cv::Point2f* target;
+    double distance;
 };
 
 #endif // BBOX_TRACKER_H_
--- test/bbox_tracker_tests.cpp
+++ test/bbox_tracker_tests.cpp
@@ -4,14 +4,93 @@
 
 #include "bbox_tracker.h"
 
-BOOST_AUTO_TEST_CASE(TrackingTest) {
 
+BOOST_AUTO_TEST_CASE(IdleTest) {
+    TrackerSetting setting({3});
+    BboxTracker tracker(std::move(setting));
+    std::vector<std::vector<FaceInfo>> testSources({
+           {},
+           {},
+           {},
+           {},
+           {}
+    });
+
+    int i = 0;
+    for (; i < testSources.size(); i++) {
+        tracker.update(testSources[i]);
+        auto trackingList = tracker.getTrackingList();
+
+        BOOST_CHECK_EQUAL(trackingList.size(), 0);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(TrackingUpdateTest) {
+    TrackerSetting setting({3});
+    BboxTracker tracker(std::move(setting));
+    std::vector<std::vector<FaceInfo>> testSources({
+        {{1, 1, 1, 1}},
+        {{2, 2, 2, 2}},
+        {{3, 3, 3, 3}}
+    });
+
+    for (const auto& faces : testSources) {
+        tracker.update(faces);
+        auto trackingList = tracker.getTrackingList();
+
+        BOOST_CHECK_EQUAL(trackingList.size(), 1);
+    }
+
+    BOOST_CHECK_EQUAL(tracker.getTrackingList()[0].midpoint.x, 3);
+    BOOST_CHECK_EQUAL(tracker.getTrackingList()[0].midpoint.y, 3);
 }
 
 BOOST_AUTO_TEST_CASE(NewAppearTest) {
+    TrackerSetting setting({3});
+    BboxTracker tracker(std::move(setting));
+    std::vector<std::vector<FaceInfo>> testSources({
+           {{1, 1, 1, 1}},
+           {{2, 2, 2, 2}},
+           {{3, 3, 3, 3}},
+           {{3,3,3,4}, {1,1,1,1}}
+    });
 
+    int i = 0;
+    for (; i < 3; i++) {
+        tracker.update(testSources[i]);
+        auto trackingList = tracker.getTrackingList();
+
+        BOOST_CHECK_EQUAL(trackingList.size(), 1);
+    }
+
+    tracker.update(testSources[i]);
+    auto trackingList = tracker.getTrackingList();
+    BOOST_CHECK_EQUAL(trackingList.size(), 2);
 }
 
 BOOST_AUTO_TEST_CASE(DisappearTest) {
+    TrackerSetting setting({3});
+    BboxTracker tracker(std::move(setting));
+    std::vector<std::vector<FaceInfo>> testSources({
+           {{1, 1, 1, 1}},
+           {{2, 2, 2, 2}},
+           {{3, 3, 3, 3}},
+           {},
+           {},
+           {},
+           {}
+    });
 
+    int i = 0;
+    for (; i < 3; i++) {
+        tracker.update(testSources[i]);
+    }
+    auto trackingList = tracker.getTrackingList();
+    BOOST_CHECK_EQUAL(trackingList.size(), 1);
+
+    for (; i < testSources.size(); i++) {
+        tracker.update(testSources[i]);
+    }
+    trackingList = tracker.getTrackingList();
+    BOOST_CHECK_EQUAL(trackingList.size(), 0);
 }
